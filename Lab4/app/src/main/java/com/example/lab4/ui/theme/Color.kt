39ef446b039d7ea90626ca92be8dbaf3eda9e0b8
Javier Lopez <lop23415@uvg.edu.gt>
2024-08-04 23:15:04 -0600

    Lab#4
    Lab#4 Frontend

---
+++ Lab4/app/src/main/java/com/example/lab4/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.lab4.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF247434)
+val PurpleGrey80 = Color(0xFF247434)
+val Pink80 = Color(0xFF247434)
+
+val Purple40 = Color(0xFF247434)
+val PurpleGrey40 = Color(0xFF247434)
+val Pink40 = Color(0xFF247434)
